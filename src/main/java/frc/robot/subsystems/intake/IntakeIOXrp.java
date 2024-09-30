e30a71851e21c06eb670eec5f4f287794ed7d79e
Nilesh Agarwalla <nilesh@alum.mit.edu>
2024-09-30 19:13:59 -0400

    Commented Intake Continuous Servo Implementation

--- src/main/java/frc/robot/subsystems/intake/IntakeIOXrp.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOXrp.java
@@ -14,8 +14,20 @@ public class IntakeIOXrp implements IntakeIO {
   }
 
   public void setSpeed(double speed) {
-    // We need to scale the desired speed [-1.0 .. 1.0] to the servo "position" [0.0 .. 1.0], where
-    // "0.5" is stopped for a continuous servo
+    // In general, the 0-180 servos that are supported by XRP use a PWM range of 1000us to 2000us.
+    // The continuous servos that we bought from Amazon
+    // (https://www.amazon.com/gp/product/B092VN3MTX)
+    //   have a 900 us to 2100 us range, where 900 is max CCW, 2100 us max CW, and 1500 is stopped.
+    //
+    // Pulse Width (us) | 1000    | 1500    | 2000   |
+    //      0-180 servo | 0 deg   | 45deg   | 90deg  |
+    // continuous servo | max CCW | stopped | max CW |
+    //
+    // Since the servos operate on the same overlapping range, for the continuous servo,
+    // we need to scale the requested speed [-1.0 .. 1.0] to a servo "position" [0.0 .. 1.0], where
+    // "0.5" is stopped for a continuous servo. This will map to the correct PWM signal to the
+    // servo.
+    //
     servo.setPosition((speed + 1) / 2);
   }
 }
