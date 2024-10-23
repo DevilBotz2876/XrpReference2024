17c3a30de29353495c7c13bc95eabade3216abb0
Nilesh Agarwalla <nilesh@alum.mit.edu>
2024-10-23 13:40:15 -0400

    Add slew rate limiter to XRPMotor speed to minimize brown-outs

--- src/main/java/frc/robot/subsystems/shooter/ShooterIOXrp.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOXrp.java
@@ -1,9 +1,11 @@
 package frc.robot.subsystems.shooter;
 
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.xrp.XRPMotor;
 
 public class ShooterIOXrp implements ShooterIO {
   private final XRPMotor motor;
+  private SlewRateLimiter filter = new SlewRateLimiter(4.0);
 
   public ShooterIOXrp(int deviceNum) {
     motor = new XRPMotor(deviceNum);
@@ -14,6 +16,7 @@ public class ShooterIOXrp implements ShooterIO {
   }
 
   public void setSpeed(double speed) {
+    speed = filter.calculate(speed);
     motor.set(speed);
   }
 }
