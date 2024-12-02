43021e6f9411222f2b3a816b9da8c251c63b1ad0
Alex Chang <alexy0730@gmail.com>
2024-12-02 18:15:23 -0500

    edit drive straight to work 60% of the time

--- src/main/java/frc/robot/commands/DriveStraight.java
+++ src/main/java/frc/robot/commands/DriveStraight.java
@@ -5,7 +5,7 @@ import frc.robot.subsystems.drive.Drive;
 
 public class DriveStraight extends SequentialCommandGroup {
   private final Drive drive;
-  private final double distance;
+  private final double distance;      
 
   public DriveStraight(Drive drive, double distance) {
     this.drive = drive;
